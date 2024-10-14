572eca30c55517f1a99584def3a7b2adf9f09746
Dadben08 <Dadben08@gmail.comcleargit config --global user.email Dadben08@gmail.com>
2024-10-14 17:05:45 +0100

    commit

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -7,15 +7,19 @@ const NewsBoard = ({category}) => {
 
   useEffect(() => {
     let url =`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
-    fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+    fetch(url).then(response=> response.json()).then(data=>setArticles(data.articles));
    },[category])
 
 
   return (
     <div>
       <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>
-      {articles.map((news,index) => {
-        return <NewsItem key={index}  title={news.title} description={news.description} url={news.url} src={news.urlToImage}  />
+      {articles.map((news,index) =>{
+        return <NewsItem key={index}
+        title={news.title} 
+        description={news.description} 
+        url={news.url} 
+        src={news.urlToImage} />
           })}
     </div>
   )
